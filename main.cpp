3bef7db25c077290becacfc2218c0f5ba8b7cad2
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-12-09 15:45:13 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int number1;
+    std::cout << "Enter the first number: ";
+    std::cin >> number1;
+
+    int number2;
+    std::cout << "Enter the second number: ";
+    std::cin >> number2;
+
+    if(number1 < number2)
+        std::cout << "Smallest number: " << number1 << std::endl;
+    else if (number2 < number1)
+        std::cout << "Smallest number: " << number2 << std::endl;
+    else
+        std::cout << "The numbers are equal!" << std::endl;
 }
